e6a618d0a93f30541d5cf9541d8647bb16cfe9da
Rozi Vector <rozi.qood16@gmail.com>
2018-10-30 22:04:46 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,9 +4,7 @@ exports.run = (client, message) => {
     message.channel.startTyping();
 	const embed = new RichEmbed()
         .addField( "Pong!" ,"Time of response " + `${client.pings[0]} ms`)
-        .addField("Message trip: ", pingMessage.createdTimestamp - message.createdTimestamp)
-        .addField("API Latency: ", this.client.ping)
-		.setColor('#FAFF00');
+	.setColor('#FAFF00');
     message.channel.sendMessage(":hourglass_flowing_sand:")
     .then(message => {message.edit({ embed })});
     message.channel.stopTyping();
