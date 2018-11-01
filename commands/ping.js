6b49a177fcac901d95d088c7ef7d1361e8e1c1f1
Rozi Vector <rozi.qood16@gmail.com>
2018-11-01 10:08:59 +0700

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,19 @@ const {RichEmbed} = require('discord.js');
 exports.run = (client, message) => {
 
     message.channel.startTyping();
-	const embed = new RichEmbed()
-        .addField( "Pong!" ,"Time of response " + `${client.pings[0]} ms ` + "\nYour time response to bot "+ `${Date.now() - start} ms`)
-	.setColor('#FAFF00');
-    message.channel.sendMessage(":hourglass_flowing_sand:")
-    .then(message => {message.edit({ embed })});
+	let start = Date.now(); 
+	message.channel.sendMessage(':hourglass_flowing_sand: ').then(message => { 
+   	 let diff = (Date.now() - start); 
+   	 let API = (client.ping).toFixed(2)
+        
+        let embed = new Discord.RichEmbed()
+        .setColor("RANDOM")
+        .addField("Pong!", "Your response to bot" + `\`${diff}ms\``, true)
+        .addField("Response to API", `\`${API}ms\``, true)
+        message.edit(embed);
+      
+    });
+  }
     message.channel.stopTyping();
     
 
