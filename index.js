1d04d2b4063685e0d5ca51a1670d3646ce5ca3ee
Rozi Vector <rozi.qood16@gmail.com>
2018-11-06 18:54:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -390,8 +390,6 @@ client.on('message', function(message){
                         message.channel.startTyping();
                         let link = dem[0];
                         let desc = dar.join(" ");
-                        if (!link) 
-                        return message.channel.send("**[Error]** Type the valid URL following the command!");
                         if (!desc)
                         desc = "Link:";
                         let user = message.author;
@@ -404,6 +402,10 @@ client.on('message', function(message){
                         .setTimestamp();
                         message.channel.send(embed)
                         message.channel.stopTyping(true);
+                        if (!link) 
+                        message.channel.stopTyping(true);
+                        message.channel.send("**[Error]** Type the valid URL following the command!");
+                        
                     };
 
                 };
