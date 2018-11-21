b5f587c9b043c6f9a469fdfe817e6d9683305b19
Rozi Vector <rozi.qood16@gmail.com>
2018-11-22 06:11:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ client.on('message', function(message){
                         .setAuthor('Support our bot!', client.user.displayAvatarURL)
                         .setDescription("Here's the special perks:")
                         .setColor('#FFD800')
-                        .addField(':arrow_forward: Invite bot', `[Click here](${'https://discordapp.com/oauth2/authorize?client_id=481305515092213763&permissions=371776&scope=bot'})`, true)
+                        .addField('<:addrobot:514940409043222538> Invite bot', `[Click here](${'https://discordapp.com/oauth2/authorize?client_id=481305515092213763&permissions=371776&scope=bot'})`, true)
                         .addField(':earth_asia: Join my server', `[Click here](${'http://discord.gg/rKjFGps'})`, true)
                         .addField(':pencil: Contact us', `[Click here](${'https://goo.gl/forms/0km4ybWuVWLQ83T33'})`, true)
                         .addField('<:YouTube:506769226766483457> My Channel', `[Click here](${'https://www.youtube.com/user/rozivector1999'})`, true)
